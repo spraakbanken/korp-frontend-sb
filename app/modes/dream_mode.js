3cbebb8a44661a933ebdb42c65fa9bffb0602c42
Martin Hammarstedt <martin.hammarstedt@gu.se>
2019-11-18 12:37:59 +0100

    Add more DReaM corpora

--- app/modes/dream_mode.js
+++ app/modes/dream_mode.js
@@ -59,4 +59,124 @@ settings.corpora["dream-de-open"] = {
     }
 };
 
+settings.corpora["dream-es-open"] = {
+    id: "dream-es-open",
+    title: "DReaM-es-open",
+    description: "",
+    within: settings.defaultWithin,
+    context: {
+        "1 sentence": "1 sentence",
+        "7 sentence": "7 sentence"
+    },
+    attributes: {
+
+    },
+    structAttributes: {
+        "doc_author": {label: "doc_author"},
+        "doc_title": {label: "doc_title"},
+        "doc_glottolog_ref_id": {label: "doc_glottolog_ref_id"},
+        "doc_hhtype": {label: "doc_hhtype"},
+        "doc_inlg": {label: "doc_inlg"},
+        "doc_lgcode": {label: "doc_lgcode"},
+        "doc_macro_area": {label: "doc_macro_area"},
+        "doc_year": {label: "doc_year"}
+    }
+};
+
+settings.corpora["dream-fr-open"] = {
+    id: "dream-fr-open",
+    title: "DReaM-fr-open",
+    description: "",
+    within: settings.defaultWithin,
+    context: {
+        "1 sentence": "1 sentence",
+        "7 sentence": "7 sentence"
+    },
+    attributes: {
+
+    },
+    structAttributes: {
+        "doc_author": {label: "doc_author"},
+        "doc_title": {label: "doc_title"},
+        "doc_glottolog_ref_id": {label: "doc_glottolog_ref_id"},
+        "doc_hhtype": {label: "doc_hhtype"},
+        "doc_inlg": {label: "doc_inlg"},
+        "doc_lgcode": {label: "doc_lgcode"},
+        "doc_macro_area": {label: "doc_macro_area"},
+        "doc_year": {label: "doc_year"}
+    }
+};
+
+settings.corpora["dream-it-open"] = {
+    id: "dream-it-open",
+    title: "DReaM-it-open",
+    description: "",
+    within: settings.defaultWithin,
+    context: {
+        "1 sentence": "1 sentence",
+        "7 sentence": "7 sentence"
+    },
+    attributes: {
+
+    },
+    structAttributes: {
+        "doc_author": {label: "doc_author"},
+        "doc_title": {label: "doc_title"},
+        "doc_glottolog_ref_id": {label: "doc_glottolog_ref_id"},
+        "doc_hhtype": {label: "doc_hhtype"},
+        "doc_inlg": {label: "doc_inlg"},
+        "doc_lgcode": {label: "doc_lgcode"},
+        "doc_macro_area": {label: "doc_macro_area"},
+        "doc_year": {label: "doc_year"}
+    }
+};
+
+settings.corpora["dream-nl-open"] = {
+    id: "dream-nl-open",
+    title: "DReaM-nl-open",
+    description: "",
+    within: settings.defaultWithin,
+    context: {
+        "1 sentence": "1 sentence",
+        "7 sentence": "7 sentence"
+    },
+    attributes: {
+
+    },
+    structAttributes: {
+        "doc_author": {label: "doc_author"},
+        "doc_title": {label: "doc_title"},
+        "doc_glottolog_ref_id": {label: "doc_glottolog_ref_id"},
+        "doc_hhtype": {label: "doc_hhtype"},
+        "doc_inlg": {label: "doc_inlg"},
+        "doc_lgcode": {label: "doc_lgcode"},
+        "doc_macro_area": {label: "doc_macro_area"},
+        "doc_year": {label: "doc_year"}
+    }
+};
+
+settings.corpora["dream-ru-open"] = {
+    id: "dream-ru-open",
+    title: "DReaM-ru-open",
+    description: "",
+    within: settings.defaultWithin,
+    context: {
+        "1 sentence": "1 sentence",
+        "7 sentence": "7 sentence"
+    },
+    attributes: {
+
+    },
+    structAttributes: {
+        "doc_author": {label: "doc_author"},
+        "doc_title": {label: "doc_title"},
+        "doc_glottolog_ref_id": {label: "doc_glottolog_ref_id"},
+        "doc_hhtype": {label: "doc_hhtype"},
+        "doc_inlg": {label: "doc_inlg"},
+        "doc_lgcode": {label: "doc_lgcode"},
+        "doc_macro_area": {label: "doc_macro_area"},
+        "doc_year": {label: "doc_year"}
+    }
+};
+
 settings.corpusListing = new CorpusListing(settings.corpora);
