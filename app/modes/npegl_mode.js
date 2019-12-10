cb9919eb61a38ef1e734f9020d8a67a94b1bb06e
Johan Roxendal <johan.roxendal@gu.se>
2019-12-10 13:37:11 +0100

    first stab at npegl cat chunking

--- app/modes/npegl_mode.js
+++ app/modes/npegl_mode.js
@@ -15,37 +15,58 @@ var textWithin = {
 };
 
 npegl = {};
-npegl.e_cat = {label: "e_cat", isStructAttr: true, order: 10};
-npegl.e_features_degrval = {label: "e_features_degrval", isStructAttr: true, order: 20};
-npegl.e_features_declval = {label: "e_features_declval", isStructAttr: true, order: 20};
-npegl.e_features_nounsem = {label: "e_features_nounsem", isStructAttr: true, order: 20};
-npegl.e_features_genprole = {label: "e_features_genprole", isStructAttr: true, order: 20};
-npegl.e_features_decl = {label: "e_features_decl", isStructAttr: true, order: 20};
-npegl.e_features_degr = {label: "e_features_degr", isStructAttr: true, order: 20};
-npegl.e_features_adjsem = {label: "e_features_adjsem", isStructAttr: true, order: 20};
-npegl.e_tags_nounsuffval = {label: "e_tags_nounsuffval", isStructAttr: true, order: 20};
-npegl.e_tags_nounlessval = {label: "e_tags_nounlessval", isStructAttr: true, order: 20};
-npegl.e_tags_noundrvval = {label: "e_tags_noundrvval", isStructAttr: true, order: 20};
-npegl.e_indices_ismoddedidx = {label: "e_indices_ismoddedidx", isStructAttr: true, order: 30};
-npegl.e_indices_modsidx = {label: "e_indices_modsidx", isStructAttr: true, order: 40};
-npegl.e_lemma = {label: "lemma", isStructAttr: true, order: 50};
-npegl.e_n = {label: "e_n", isStructAttr: true, order: 50};
-npegl.e_tags_defval = {label: "e_tags_defval", isStructAttr: true, order: 60};
+npegl.e_cat = {label: "e_cat", isStructAttr: false, order: 10, stats_stringify: (values) => {
+    values = Array.from(values)
+    let groups = []
+    // values = values.reverse()
+    let take = (allOfVal, fromArray) => {
+        let output = []
+        while(fromArray.length && fromArray[0] === allOfVal) {
+            output = output.concat(fromArray.splice(0, 1))
+        }
+        return output
+    }
 
-npegl.np_order = {label: "np_order", isStructAttr: true, order: 110};
-npegl.np_language = {label: "np_language", isStructAttr: true, order: 110};
-npegl.np_number = {label: "np_number", isStructAttr: true, order: 110};
-npegl.np_case = {label: "np_case", isStructAttr: true, order: 110};
-npegl.np_grammatical_function = {label: "np_grammatical_function", isStructAttr: true, order: 120};
-npegl.np_gender = {label: "gender", isStructAttr: true, order: 130, extendedComponent: "structServiceSelect"};
-npegl.np_referential_status = {label: "np_referential_status", isStructAttr: true, order: 140};
-npegl.np_comments = {label: "commentaries", isStructAttr: true, order: 150};
-npegl.np_annotation_time = {label: "np_annotation_time", isStructAttr: true, order: 160};
-npegl.np_annotator = {label: "np_annotator", isStructAttr: true, order: 170};
-npegl.np_degree_of_interest = {label: "np_degree_of_interest", isStructAttr: true, order: 180, extendedComponent: "structServiceSelect"};
-npegl.np_corpus_unit_id = {label: "np_corpus_unit_id", isStructAttr: true, order: 190};
-npegl.np_db_item_id = {label: "id", isStructAttr: true, order: 200};
-npegl.np_verified = {label: "np_verified", isStructAttr: true, order: 110};
+    while(values.length) {
+        let group = take(values[0], values)
+        if(group.length == 1) {
+            groups.push(group[0].split(":")[0])
+        } else {
+            groups.push("[" + group.map((item) => item.split(":")[0]).join(" ") + "]" )
+        }
+    }
+    return groups.join(" ")
+}};
+npegl.e_features_degrval = {label: "e_features_degrval", isStructAttr: false, order: 20};
+npegl.e_features_declval = {label: "e_features_declval", isStructAttr: false, order: 20};
+npegl.e_features_nounsem = {label: "e_features_nounsem", isStructAttr: false, order: 20};
+npegl.e_features_genprole = {label: "e_features_genprole", isStructAttr: false, order: 20};
+npegl.e_features_decl = {label: "e_features_decl", isStructAttr: false, order: 20};
+npegl.e_features_degr = {label: "e_features_degr", isStructAttr: false, order: 20};
+npegl.e_features_adjsem = {label: "e_features_adjsem", isStructAttr: false, order: 20};
+npegl.e_tags_nounsuffval = {label: "e_tags_nounsuffval", isStructAttr: false, order: 20};
+npegl.e_tags_nounlessval = {label: "e_tags_nounlessval", isStructAttr: false, order: 20};
+npegl.e_tags_noundrvval = {label: "e_tags_noundrvval", isStructAttr: false, order: 20};
+npegl.e_indices_ismoddedidx = {label: "e_indices_ismoddedidx", isStructAttr: false, order: 30};
+npegl.e_indices_modsidx = {label: "e_indices_modsidx", isStructAttr: false, order: 40};
+npegl.e_lemma = {label: "lemma", isStructAttr: false, order: 50};
+npegl.e_n = {label: "e_n", isStructAttr: false, order: 50};
+npegl.e_tags_defval = {label: "e_tags_defval", isStructAttr: false, order: 60};
+
+npegl.np_order = {label: "np_order", isStructAttr: false, order: 110};
+npegl.np_language = {label: "np_language", isStructAttr: false, order: 110};
+npegl.np_number = {label: "np_number", isStructAttr: false, order: 110};
+npegl.np_case = {label: "np_case", isStructAttr: false, order: 110};
+npegl.np_grammatical_function = {label: "np_grammatical_function", isStructAttr: false, order: 120};
+npegl.np_gender = {label: "gender", isStructAttr: false, order: 130, extendedComponent: "structServiceSelect"};
+npegl.np_referential_status = {label: "np_referential_status", isStructAttr: false, order: 140};
+npegl.np_comments = {label: "commentaries", isStructAttr: false, order: 150};
+npegl.np_annotation_time = {label: "np_annotation_time", isStructAttr: false, order: 160};
+npegl.np_annotator = {label: "np_annotator", isStructAttr: false, order: 170};
+npegl.np_degree_of_interest = {label: "np_degree_of_interest", isStructAttr: false, order: 180, extendedComponent: "structServiceSelect"};
+npegl.np_corpus_unit_id = {label: "np_corpus_unit_id", isStructAttr: false, order: 190};
+npegl.np_db_item_id = {label: "id", isStructAttr: false, order: 200};
+npegl.np_verified = {label: "np_verified", isStructAttr: false, order: 110};
 
 settings.corpora["npegl-eng"] = {
     id: "npegl-eng",
