c8db6a0bb7e1b980f0f1b7ef7e23e557d56f2744
Maria Öhrman <maria.ohrman@gu.se>
2021-06-22 15:19:48 +0200

    export default in stringify

--- app/custom/stringify.js
+++ app/custom/stringify.js
@@ -1,4 +1,4 @@
-export const stringifyFunctions = {
+export default {
     sense: (sense) => util.saldoToString(sense, true),
     lemgram: (str) => util.lemgramToString(str, true),
     complemgram: (str) => str.split('+').map((lemgram) => util.lemgramToString(lemgram, true)).join('+')
