a23c6a3dc6c228b2079f008b7562d5219d997fe6
Maria Öhrman <maria.ohrman@gu.se>
2023-04-26 14:37:00 +0200

    add a sidebar component, copyRowButton

--- app/custom/sidebar.js
+++ app/custom/sidebar.js
@@ -165,7 +165,17 @@ export default {
                         val: value,
                     })
                 }
-            }     
+            }
        }]
-    })
-}   
+    }),
+    copyRowButton: (options = {}) => ({
+        template: `<span class="cursor-pointer" ng-click="click()"><i class="fa-solid fa-copy"></i> Kopiera rad</span>`,
+        controller: ["$scope", function($scope) {
+            $scope.copyStr = _.map(options["attributes"] || ["word"], (attribute) =>
+                _.map($scope.tokens, (token) => token[attribute]).join("\t")
+            ).join("\n")
+
+            $scope.click = () => navigator.clipboard.writeText($scope.copyStr)
+       }]
+    }),
+}
