7d3f78fdee95fcc38d37e9f97ce5c91019c77bd5
Arild Matsson <arild.matsson@gu.se>
2025-03-25 14:20:57 +0100

    revert: "refactor: use folder ids in search examples"
    This reverts commit 26be8961b48dc79d450b83629eb526a522aa12fa.
    
    Folder ids are only handled correctly when used for first load.

--- app/modes/default_mode.js
+++ app/modes/default_mode.js
@@ -6,7 +6,7 @@ settings["frontpage"]["examples"] = [
     label: 'Former av ordet "fågel" i skönlitteratur',
     params: {
       search: "lemgram|fågel..nn.1",
-      corpus: "novels",
+      corpus: "aspacsv,rom99,romg,romi,romii,saltnld-sv,storsuc",
     },
     hint: "Öppna fliken Statistik för att se en fördelning av ordformer",
   },
@@ -19,7 +19,7 @@ settings["frontpage"]["examples"] = [
       search_tab: 1,
       search: "cqp",
       cqp: '[word ^= "covid" & word != "covid-19"]',
-      corpus: "newstexts.svtnews,newstexts.webnews,attasidor,da",
+      corpus: "attasidor,da,svt-2004,svt-2005,svt-2006,svt-2007,svt-2008,svt-2009,svt-2010,svt-2011,svt-2012,svt-2013,svt-2014,svt-2015,svt-2016,svt-2017,svt-2018,svt-2019,svt-2020,svt-2021,svt-2022,svt-2023,svt-nodate,webbnyheter2001,webbnyheter2002,webbnyheter2003,webbnyheter2004,webbnyheter2005,webbnyheter2006,webbnyheter2007,webbnyheter2008,webbnyheter2009,webbnyheter2010,webbnyheter2011,webbnyheter2012,webbnyheter2013",
     },
   },
   {
@@ -30,7 +30,7 @@ settings["frontpage"]["examples"] = [
     params: {
       search_tab: 2,
       search: 'cqp|a:[pos = "AB"] [word=a.word]+',
-      corpus: "strindberg",
+      corpus: "strindbergbrev,strindbergromaner",
     },
   },
   {
@@ -61,7 +61,7 @@ settings["frontpage"]["examples"] = [
     params: {
       suffix: 1,
       search: "lemgram|bil..nn.1",
-      corpus: "medical",
+      corpus: "diabetolog,lt1996,lt1997,lt1998,lt1999,lt2000,lt2001,lt2002,lt2003,lt2004,lt2005,lt2006,smittskydd",
     },
   },
   {
@@ -70,7 +70,7 @@ settings["frontpage"]["examples"] = [
       search_tab: 1,
       search: "cqp",
       cqp: '[lemma contains "örfil"]',
-      corpus: "fisk",
+      corpus: "abounderrattelser2012,abounderrattelser2013,astra1960-1979,astranova,at2012,barnlitteratur,borgabladet,bullen,fanbararen,finsktidskrift,fnb1999,fnb2000,forumfeot,fsbbloggvuxna,fsbessaistik,fsbsakprosa,fsbskonlit1960-1999,fsbskonlit2000tal,hankeiten,hanken,hbl1991,hbl1998,hbl1999,hbl20122013,hbl2014,informationstidningar,jakobstadstidning1999,jakobstadstidning2000,jft,kallan,lagtexter,magmakolumner,meddelanden,myndighet,nyaargus,osterbottenstidning2011,osterbottenstidning2012,osterbottenstidning2013,osterbottenstidning2014,ostranyland,pargaskungorelser2011,pargaskungorelser2012,propositioner,studentbladet,svenskbygden,sydosterbotten2012,sydosterbotten2013,sydosterbotten2014,ungdomslitteratur,vasabladet1991,vasabladet2012,vasabladet2013,vasabladet2014,vastranyland,sydosterbotten2011,sydosterbotten2010",
       reading_mode: 1,
     },
   },
@@ -81,7 +81,7 @@ settings["frontpage"]["examples"] = [
       search_tab: 1,
       search: "cqp",
       cqp: '[(word = "orkar" | word = "ids" | word = "tyar")]',
-      corpus: "socialmedia.bloggmix",
+      corpus: "bloggmix1998,bloggmix1999,bloggmix2000,bloggmix2001,bloggmix2002,bloggmix2003,bloggmix2004,bloggmix2005,bloggmix2006,bloggmix2007,bloggmix2008,bloggmix2009,bloggmix2010,bloggmix2011,bloggmix2012,bloggmix2013,bloggmix2014,bloggmix2015,bloggmix2016,bloggmix2017,bloggmixodat",
       result_tab: 2,
     },
   },
@@ -90,7 +90,7 @@ settings["frontpage"]["examples"] = [
     params: {
       search: "lemgram|björn..nn.1",
       isCaseInsensitive: 1,
-      corpus: "novels",
+      corpus: "aspacsv,rom99,romg,romi,romii,saltnld-sv,storsuc",
       result_tab: 3,
     },
   },
@@ -121,7 +121,7 @@ settings["frontpage"]["examples"] = [
       search_tab: 1,
       search: "cqp",
       cqp: '[word _= "krig"]',
-      corpus: "newstexts.gp,newstexts.webnews,newstexts.svtnews,attasidor,da,press95,press96,press97,press98",
+      corpus: "attasidor,da,gp1994,gp2001,gp2002,gp2003,gp2004,gp2005,gp2006,gp2007,gp2008,gp2009,gp2010,gp2011,gp2012,gp2013,gp2d,press95,press96,press97,press98,svt-2004,svt-2005,svt-2006,svt-2007,svt-2008,svt-2009,svt-2010,svt-2011,svt-2012,svt-2013,svt-2014,svt-2015,svt-2016,svt-2017,svt-2018,svt-2019,svt-2020,svt-2021,svt-2022,svt-2023,webbnyheter2001,webbnyheter2002,webbnyheter2003,webbnyheter2004,webbnyheter2005,webbnyheter2006,webbnyheter2007,webbnyheter2008,webbnyheter2009,webbnyheter2010,webbnyheter2011,webbnyheter2012,webbnyheter2013",
       result_tab: 2,
     },
   },
