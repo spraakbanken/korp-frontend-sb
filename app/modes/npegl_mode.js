5b9b0de972176ae055a9510096a85c25fd8ad107
Arild Matsson <arild.matsson@gu.se>
2025-07-02 15:48:20 +0200

    fix: NPEGL use statistics_postprocess hook
    The StatsProxy override doesnt work since https://github.com/spraakbanken/korp-frontend/commit/98f93984

--- app/modes/npegl_mode.js
+++ app/modes/npegl_mode.js
@@ -1,49 +1,44 @@
-import statsProxyFactory, { StatsProxy } from "@/backend/stats-proxy";
 import { catToString } from "custom/npegl.js";
 
-class NpeglStatsProxy extends StatsProxy {
-    async makeRequest(cqp, callback) {
-        const { rows, columns, params } = await super.makeRequest(cqp, callback);
+settings["stats_rewrite"] = (result) => {
+    const { rows, params } = result;
 
-        // Group rows that have the same representation of the e_cat attribute (e.g. [a:1 a:1] == [a:1])
-        // Skip the totals row
-        const groups = _.groupBy(rows.slice(1), (row) => {
-            const values = swapLevels(row.statsValues);
-            const repr = (attr) => (attr == "e_cat" ? catToString(values[attr].flat()) : values[attr].flat().join(" "));
-            return params.reduceVals.map(repr).join("/");
-        });
+    // Group rows that have the same representation of the e_cat attribute (e.g. [a:1 a:1] == [a:1])
+    // Skip the totals row
+    const groups = _.groupBy(rows.slice(1), (row) => {
+        const values = swapLevels(row.statsValues);
+        const repr = (attr) => (attr == "e_cat" ? catToString(values[attr].flat()) : values[attr].flat().join(" "));
+        return params.reduceVals.map(repr).join("/");
+    });
 
-        // Merge the rows in each group
-        const output = Object.values(groups).map((group) =>
-            group.reduce(
-                (agg, row) => {
-                    // Sum up frequencies
-                    agg.total = add(agg.total, row.total);
-                    for (const corpusId in row.count) {
-                        agg.count[corpusId] = add(agg.count[corpusId] || [0, 0], row.count[corpusId]);
-                    }
+    // Merge the rows in each group
+    const output = Object.values(groups).map((group) =>
+        group.reduce(
+            (agg, row) => {
+                // Sum up frequencies
+                agg.total = add(agg.total, row.total);
+                for (const corpusId in row.count) {
+                    agg.count[corpusId] = add(agg.count[corpusId] || [0, 0], row.count[corpusId]);
+                }
 
-                    // we have to let Korp know that the cqp expression we need to create
-                    // is like [] | [] | [], which Korp doesn't have a natural way to handle.
-                    agg.isPhraseLevelDisjunction = true;
-                    // Usually a Record[], but for NPEGL a Record[][] to enable phrase-level disjunction.
-                    agg.statsValues.push(row.statsValues);
+                // we have to let Korp know that the cqp expression we need to create
+                // is like [] | [] | [], which Korp doesn't have a natural way to handle.
+                agg.isPhraseLevelDisjunction = true;
+                // Usually a Record[], but for NPEGL a Record[][] to enable phrase-level disjunction.
+                agg.statsValues.push(row.statsValues);
 
-                    // Include other things from row that are not yet in agg (rowId, formattedValue?)
-                    return { ...row, ...agg };
-                },
-                { total: [0, 0], count: {}, statsValues: [] }
-            )
-        );
+                // Include other things from row that are not yet in agg (rowId, formattedValue?)
+                return { ...row, ...agg };
+            },
+            { total: [0, 0], count: {}, statsValues: [] }
+        )
+    );
 
-        // Add total row on top.
-        output.unshift(rows[0]);
+    // Add total row on top.
+    output.unshift(rows[0]);
 
-        return { rows: output, columns, params };
-    }
-}
-
-statsProxyFactory.setClass(NpeglStatsProxy);
+    return { rows: output, params };
+};
 
 /** Sum the abs/rel frequencies of two cells */
 const add = (arr1, arr2) => [arr1[0] + arr2[0], arr1[1] + arr2[1]];
